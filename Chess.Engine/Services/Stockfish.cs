325306229dbcbb3216badec2b23d218534131789
AndreiBcs <andreibichis1@gmail.com>
2026-08-14 19:27:02 +0300

    added stockfish.exe

---
+++ Chess.Engine/Services/Stockfish.cs
@@ -0,0 +1,15 @@
+﻿namespace Chess.Engine.Services;
+
+public class Stockfish
+{
+    public Stockfish()
+    {
+        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Executable", "stockfish.exe");
+
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException("stockfish.exe not found", path);
+        }
+
+    }
+}
---
+++ Chess.Engine/Start.cs
@@ -0,0 +1,11 @@
+﻿using Chess.Engine.Services;
+
+namespace Chess.Engine;
+
+public static class Start
+{
+    public static void Main()
+    {
+        var s = new Stockfish();
+    }
+}
