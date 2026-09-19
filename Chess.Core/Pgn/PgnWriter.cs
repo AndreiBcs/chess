d20ec223d0228fae2506e2e5da7f4995e692978e
AndreiBcs <andreibichis1@gmail.com>
2026-09-19 16:09:44 +0300

    pgn writer

--- Chess.Core/Pgn/PgnWriter.cs
+++ Chess.Core/Pgn/PgnWriter.cs
@@ -14,7 +14,76 @@ public static class PgnWriter
             var status = snapshot.PreviousMoveStatus;
             var move = snapshot.PreviousMove;
             
-            // todo
+            sb.Append(snapshot.FullMoveCounter).Append(". ");
+
+            if (status is { IsCastling: true, CastlingRights: not null })
+            {
+                if (status.CastlingRights?.LetterId.ToString().ToLower() is "k")
+                {
+                    sb.Append("O-O ");
+                }
+                else if (status.CastlingRights?.LetterId.ToString().ToLower() is "q")
+                {
+                    sb.Append("O-O-O ");
+                }
+            }
+            else
+            {
+                if (status.IsCapture)
+                {
+                    if (status.IsPawnMove)
+                    {
+                        sb.Append(move.From.ToString()[0])
+                            .Append('x')
+                            .Append(move.To.ToString());
+                    }
+                    else
+                    {
+                        sb.Append(snapshot.Board.GetPiece(move.From)!.LetterId)
+                            .Append('x')
+                            .Append(move.To.ToString());
+                    }
+                }
+                else
+                {
+                    if (status.IsPawnMove)
+                    {
+                        sb.Append(move.From.ToString());
+                    }
+                    else
+                    {
+                        sb.Append(snapshot.Board.GetPiece(move.From)!.LetterId)
+                            .Append(move.To.ToString());
+                    }
+                }
+
+                if (snapshot.Status is not GameStatus.InProgress)
+                {
+                    if (snapshot.Status is GameStatus.BlackWon)
+                    {
+                        sb.Append("# 0-1");
+                        break;
+                    }
+
+                    if (snapshot.Status is GameStatus.WhiteWon)
+                    {
+                        sb.Append("# 1-0");
+                        break;
+                    }
+                    
+                    sb.Append(" 1/2-1/2");
+                    break;
+                }
+                
+                if (status.IsCheck)
+                {
+                    sb.Append("+ ");
+                }
+                else
+                {
+                    sb.Append(' ');
+                }
+            }
         }
         
         return sb.ToString();
